3f9b1596df6c40c9127c76a30a6e4b0c2722901f
saimon.merdok@gmail.com <saimon.merdok@gmail.com>
2020-09-10 21:53:31 +0300

    Первая итерация

---
+++ Fitness.bl/Model/User.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Fitness.bl.Model
+{/// <summary>
+/// Пользователь
+/// </summary>
+   public class User
+    {
+        #region Свойства
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// Вес
+        /// </summary>
+        public double Weigth { get; set; }
+        /// <summary>
+        /// Рост
+        /// </summary>
+        public double Height { get; set; }
+        #endregion 
+        /// <summary>
+        /// Создать нового пользователя
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="gender">Пол</param>
+        /// <param name="birhtDate">Дата рождения</param>
+        /// <param name="weigth">Вес</param>
+        /// <param name="height">Рост</param>
+        public User(string name,Gender gender, DateTime birhtDate,double weigth, double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(name));
+            }
+            if(Gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть null", nameof(gender));
+            }
+            if(birhtDate<DateTime.Parse("01.01.1900") || birhtDate >=DateTime.Now)
+            {
+                throw new ArgumentNullException("Невозможная дата рождения", nameof(birhtDate));
+            }
+            if(weigth<=0)
+            {
+                throw new ArgumentNullException("Вес не может быть меньше лиьо равен 0", nameof(weigth));
+            }
+            if(height<=0)
+            {
+                throw new ArgumentNullException("Рост не может быть меньше или равен нулю", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birhtDate;
+            Weigth = weigth;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.bl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Fitness.bl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fitness.bl")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cbbf99d2-1d39-4b21-8cfc-3b6150a11c6a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
